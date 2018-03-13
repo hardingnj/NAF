ccaf1759eec5b29f31525151d461353bde47e38b
Nicholas Harding <njh@well.ox.ac.uk>
2018-03-13 14:19:14 +0000

    Incorp feedback
    Ranking 999999 is ok, as we can conditionally format
    No need for active column

--- scripts/prep_upload_csv.py
+++ scripts/prep_upload_csv.py
@@ -7,24 +7,23 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv(fn, index_col=0)
+# apply phi cutoff
+df = df.loc[df.phi < snakemake.params.phi_limit]
 
 rank = 1
 url_path = "https://member.thenaf.net/index.php?module=NAF&type=tournamentinfo&uid={nafnum}||{value}"
 
 rank_list = []
 for phi in df.phi:
-  if phi > snakemake.params.phi_limit:
+  if phi > snakemake.params.phi_active:
     rank_list.append(999999)
   else:
-    rank_list.append(str(rank))
+    rank_list.append(rank)
     rank += 1
 
-df["active"] = np.where(df.phi > 100, "Inactive", "Active") 
 df["rank"] = rank_list
-
 df["coachname"] = df.apply(lambda y: url_path.format(nafnum=y.naf_number, value=y.coach), axis=1)
 df["rating"] = df["value"]
 
-dfq = df[["rank", "coachname", "naf_number", "race", "nation", "mu", "phi", "rating", "active"]]
-
+dfq = df[["rank", "coachname", "naf_number", "race", "nation", "mu", "phi", "rating"]]
 dfq.to_csv(outfn, index=False, header=True, float_format="%.1f")
