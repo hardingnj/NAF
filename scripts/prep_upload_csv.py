6c87d4a98736ba8b2aa0d91ad8163f11537d24d8
Nick Harding <njh@well.ox.ac.uk>
2018-03-28 10:26:08 +0100

    Fix typo/bug

--- scripts/prep_upload_csv.py
+++ scripts/prep_upload_csv.py
@@ -36,6 +36,6 @@ df["racerank"] = race_rank_list
 df["coachname"] = df.apply(lambda y: url_path.format(nafnum=y.naf_number, value=y.coach), axis=1)
 df["rating"] = df["value"]
 
-dfq = df[["rank", "racerank", "coachname", "naf_number", "race", "nation", "mu", "phi", "rating", "active"]]
+dfq = df[["rank", "racerank", "coachname", "naf_number", "race", "nation", "mu", "phi", "rating"]]
 
 dfq.to_csv(outfn, index=False, header=True, float_format="%.1f")
