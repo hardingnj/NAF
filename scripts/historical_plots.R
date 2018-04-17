495aacbb2756829673a21f61dcc606ca67cc3457
Stephen Pearce <spearce@me.com>
2018-04-17 23:05:39 +1000

    tidied, refactored and some comments

--- scripts/historical_plots.R
+++ scripts/historical_plots.R
@@ -1,34 +1,73 @@
 library(tidyverse)
 library(rhdf5)
 
-d = H5Fopen("~/Downloads/rankings.h5")
+# Tidies coach data from .h5 file for plotting
+# Rows are the races and columns the dates
+tidy_data <- function(data, r_name, c_name) {
+  data <- data %>% 
+    as_data_frame() %>% 
+    set_colnames(c_name) %>% 
+    mutate(race = r_name) %>% 
+    gather(date, ranking, -race) %>% 
+    filter(!is.nan(ranking)) %>% 
+    mutate(date = lubridate::ymd(date))
+}
 
-d$"coaches/Pipey" %>% 
-  map(
-    ~{as.data.frame(.) %>% 
-        `colnames<-`(d$date) %>% 
-        `rownames<-`(d$race_ids) %>% 
-        mutate(race = rownames(.)) %>% 
-        gather(date, ranking, -race) %>% 
-        filter(!is.nan(ranking)) %>% 
-        mutate(date = lubridate::ymd(date))
-    } 
-  ) %>%  
-  bind_cols() %>% 
-  select(date, race, mu = "ranking", phi="ranking1") %>% 
-  group_by(race) %>% 
-  arrange(date) %>% 
-  mutate(next_date = lead(date, default = NA)) %>% 
-  ggplot(aes(date, mu-2.5*phi, fill = race)) + 
-  geom_rect(data = function(d) {e = 70; data_frame(date = rep(d$date, each = e+1), next_date = rep(d$next_date, each = e+1), race = rep(d$race, each = e+1), mu = rep(d$mu, each=e+1), phi = rep(d$phi, each = e+1), y =map2(d$mu, d$phi, ~seq(.x-.y*3, .x+.y*3, length.out = e+1)) %>% unlist) %>% group_by(date,race)%>% mutate(f = dnorm(y, mean=mu, sd=phi), width = y-lead(y))}, aes(xmin= date, xmax = next_date, ymin = y-width/2, ymax =y+(width/2) , fill=race, alpha = f, group=race), height = 3, colour=NA) + 
-  geom_point(aes(y=mu, colour = race), alpha = 0.4, size = 0.5) + 
-  facet_wrap(~race) + 
-  geom_point(span = 0.2, colour="grey70", se=F, size = 0.5)+
-  geom_smooth(span = 0.2, colour= "black", se = F) + 
-  theme_nufflytics() + 
-  xlab(NULL) + 
-  ylab("mu") + 
-  ggtitle("Rating changes","Pipey") + 
-  viridis::scale_color_viridis(discrete = T) + 
-  viridis::scale_fill_viridis(discrete = T) +
-  theme(legend.position= "none")
+# Given a rankings.h5 file and a coach name, extracts the coach data
+extract_coach_data <- function(coach, file) {
+  accessor = paste0("coaches/", coach)
+  
+  data <- h5read(file, accessor) %>% 
+    map(tidy_data, h5read(file, "race_ids"), h5read(file, "date")) %>% 
+    cbind() %>% 
+    bind_cols() %>% 
+    select(race, date, mu = "ranking", phi = "ranking1") %>% 
+    group_by(race) %>% 
+    arrange(date) %>% 
+    mutate(last_date = lag(date))
+  
+  H5close()
+  
+  data
+}
+
+# Produces the underlying data for the density plots.
+# For each date/race combination, calculates the normal density of the specified number of bins from mu +/- 3 phi 
+density_data <- function(d, bins = 100) {
+  dens_data <- data_frame(
+    date      = rep(d$date, each = bins), 
+    last_date = rep(d$last_date, each = bins), 
+    race      = rep(d$race, each = bins), 
+    mu        = rep(d$mu, each=bins), 
+    phi       = rep(d$phi, each = bins), 
+    y         = map2(d$mu, d$phi, ~seq(.x-.y*3, .x+.y*3, length.out = bins)) %>% unlist # $bins points evenly spaced mu +/- 3 phi  
+  )
+  
+  dens_data %>% 
+    group_by(date,race) %>% 
+    mutate(dens = dnorm(y, mean=mu, sd=phi)) # normal density at each bin
+}
+
+top10_coaches <-c("Jimjimany", "AndyDavo", "Pipey", "straume", "Phoenix11", "Jeff", "delevus", "Karaak", "delevus", "Harti")
+
+# Given a coach name, make a plot of historical ratings
+plot_coach <- function(coach) {
+  extract_coach_data(coach, "../output/rankings.h5") %>% 
+    ggplot(aes(x = date, fill = race)) +
+    geom_rect( # Hidden skill probability distribution
+      data = function(d) density_data(d, bins = 40), 
+      aes(xmin = last_date, xmax = date, ymin = y, ymax = lead(y), fill = race, alpha = dens, group=race), 
+      colour=NA
+    ) +
+    geom_point(aes(y = mu, colour = race), alpha = 0.2, size = 0.5) + # Points to show mu
+    geom_point(aes(y = mu-(2.5*phi)), colour = "grey40", size = 0.5) + # Points to show rating
+    geom_smooth(aes(y = mu-(2.5*phi)), colour = "black", se = F, span = 0.2) + # Smoothed ranking
+    labs(title = "Glicko Ratings", subtitle = coach, x = NULL, y = "Rating") +
+    facet_wrap(~race) +
+    viridis::scale_color_viridis(discrete = T) + 
+    viridis::scale_fill_viridis(discrete = T) +
+    theme_linedraw() +
+    theme(legend.position= "none")
+}
+
+plots <- map(top10_coaches, plot_coach)
